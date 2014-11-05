d7b36ee0c55bb63ef84361dd52a7141c878e46f5
Zhe Xie <zhexie@usc.edu>
2014-11-04 20:44:22 -0800

    FB Q11 - updated

--- Kth Smallest Element in Sorted 2D Array.cpp
+++ Kth Smallest Element in Sorted 2D Array.cpp
@@ -10,52 +10,64 @@
         32, 33, 39, 50
 	The 3rd smallest element is 20 and 7th smallest element is 30
  */
-#include<vector>
-#include<queue>
+
+// LeetCode: merge K linked list
+
+// maintain a min heap which keeps elements in the first column at the beginning
+// then we can get the min, and then push the next of min into the heap
+// time complexity: O(klogn)
+
 #include<iostream>
+#include<queue>
 using namespace std;
 
-class solution_fbkme{
-public:
-	int findKthSmallest(vector<vector<int>> &matrix, int k){
-		int m = matrix.size();
-		if(m == 0) return 0;
-		int n = matrix[0].size();
-		if(m * n < k) k = k % (m*n);
+// need to use heap node to get its next element at the same row, int pointer doesn't work 
+struct node{
+	int val;
+	int row;
+	int col;
+	node(int v, int r, int c): val(v), row(r), col(c){}
+};
 
-		priority_queue<int, vector<int>> pq; // max heap default
-		for(int i = 0; i < m; i++){
-			for(int j = 0; j < n; j++){
-				if(pq.size() < k) pq.push(matrix[i][j]);
-				else{
-					if(matrix[i][j] < pq.top()){
-						pq.pop();
-						pq.push(matrix[i][j]);
-					}
-				}
+struct cmp{
+	bool operator () (const node *a, const node *b){
+		return b->val < a->val;
+	}
+};
+
+class solution_fbks{
+public:
+	int findKthSmallest(int matrix[4][4], int n, int k){
+		priority_queue<node*, vector<node*>, cmp> minheap;
+
+		// first column of the matrix
+		for(int i = 0; i < n; i++)
+			minheap.push(new node(matrix[i][0], i, 0));
+
+		// pop off the first k-1 smallest elements
+		for(int i = 1; i < k; i++){
+			if(minheap.empty()) return -1; // less than k elements
+
+			node *tmp = minheap.top();
+			minheap.pop();
+			if(tmp->col + 1 < n){
+				int r = tmp->row;
+				int c = tmp->col+1;
+				minheap.push(new node(matrix[r][c], r, c));
 			}
 		}
-
-		cout << pq.top() << endl;
+		return minheap.top()->val;
 	}
 };
 
 int main(){
-	solution_fbkme *obj = new solution_fbkme();
-	int a1[] = {10, 20, 30, 40};
-	int a2[] = {15, 25, 35, 45};
-	int a3[] = {24, 29, 37, 48};
-	int a4[] = {32, 33, 39, 50};
-	vector<int> v1 (a1, a1+4);
-	vector<int> v2 (a2, a2+4);
-	vector<int> v3 (a3, a3+4);
-	vector<int> v4 (a4, a4+4);
-	vector<vector<int>> vv;
-	vv.push_back(v1);
-	vv.push_back(v2);
-	vv.push_back(v3);
-	vv.push_back(v4);
-	obj->findKthSmallest(vv, 3);
-	obj->findKthSmallest(vv, 20);
+	solution_fbks *obj = new solution_fbks();
+	int matrix[4][4] = {{10, 20, 30, 40},
+						{15, 25, 35, 45},
+						{25, 29, 37, 48},
+						{32, 33, 39, 50},
+						};
+	cout << obj->findKthSmallest(matrix, 4, 3) << endl;
+	cout << obj->findKthSmallest(matrix, 4, 7) << endl;
 	return 0;
 }
